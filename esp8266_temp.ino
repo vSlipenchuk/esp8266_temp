1fc655fe7cd36c41ce252c0ceae344783e20fc27
Vladimir Slipenchuk <vslipenchuk@gmail.com>
2016-11-01 01:47:18 +0300

    added url handlers /temp /on /off

--- esp8266_temp.ino
+++ esp8266_temp.ino
@@ -5,6 +5,10 @@
 
 // Including the ESP8266 WiFi library
 #include <ESP8266WiFi.h>
+
+  // D1, D2, D3, D4, ->  GPIO 5, 4, 0, 2  (esp8266 digitalPins)
+  int D[4]={5,4,0,2};
+
 #include "DHT.h"
 
 // Uncomment one of the lines below for whatever DHT sensor type you're using!
@@ -13,8 +17,13 @@
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 // Replace with your network details
+
+#include "/mnt/c/prj/const.h"
+#ifndef WIFI
+#define WIFI
 const char* ssid = "..SSID..";
-const char* password = "...pass..";
+const char* password = "..password..";
+#endif
 
 // Web Server on port 80
 WiFiServer server(80);
@@ -34,6 +43,11 @@ void setup() {
   // Initializing serial port for debugging purposes
   Serial.begin(115200);
   delay(10);
+
+  for(int i=0;i<4;i++) {
+     pinMode(D[i],OUTPUT);
+     digitalWrite(D[i],LOW);
+     }
 
   dht.begin();
   
@@ -60,22 +74,9 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-// runs over and over again
-void loop() {
-  // Listenning for new clients
-  WiFiClient client = server.available();
-  
-  if (client) {
-    Serial.println("New client");
-    // bolean to locate when the http request ends
-    boolean blank_line = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        
-        if (c == '\n' && blank_line) {
-            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-            float h = dht.readHumidity();
+
+void read_temp() {
+float h = dht.readHumidity();
             // Read temperature as Celsius (the default)
             float t = dht.readTemperature();
             // Read temperature as Fahrenheit (isFahrenheit = true)
@@ -117,11 +118,18 @@ void loop() {
               Serial.print(" *C ");
               Serial.print(hif);
               Serial.println(" *F");
-            }
-            client.println("HTTP/1.1 200 OK");
+            }  
+}
+
+void flash_http200(WiFiClient client) {
+ client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
             client.println("Connection: close");
             client.println();
+}
+
+void flash_temp(WiFiClient client) {
+flash_http200(client);
             // your actual web page that displays temperature and humidity
             client.println("<!DOCTYPE HTML>");
             client.println("<html>");
@@ -133,14 +141,67 @@ void loop() {
             client.println(humidityTemp);
             client.println("%</h3><h3>");
             client.println("</body></html>");     
+}
+
+void do_onoff(WiFiClient client,int pin, int mode) {
+
+  flash_http200(client);
+  if (pin<1 || pin>4) {
+     client.println("ERROR USAGE pin 1..4");
+     
+    } else { // OK
+      pin=D[pin-1]; // decode
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,mode?HIGH:LOW);
+  client.print("PIN:");  client.println(pin); 
+  client.print("MODE:"); client.println(mode);
+    }
+}
+
+ 
+
+char url[80]; int l=0; int line_no=0;
+
+// runs over and over again
+void loop() {
+  // Listenning for new clients
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("New client");
+    url[0]=0; l=0; line_no=0;
+    // bolean to locate when the http request ends
+    boolean blank_line = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();        
+        if (c == '\n' && blank_line) {
+            char *u=url;
+            if (strncmp(u,"GET ",4)==0) u+=4;
+            if (strncmp(u,"/on",3)==0) { // switch on/off
+               do_onoff(client,atoi(u+3),1);
+            } 
+            else if (strncmp(u,"/off",4)==0) { // switch on/off
+               do_onoff(client,atoi(u+4),0);
+            } 
+            else if (strncmp(u,"/temp",5)==0) { // flash just temp
+              read_temp();
+              flash_http200(client);
+              client.println(celsiusTemp);
+               // send temp
+            }
+            else { // default page
+            read_temp();
+            Serial.print("Flash on URL="); Serial.println(url);
+            flash_temp(client);
+            }
             break;
         }
         if (c == '\n') {
-          // when starts reading a new line
-          blank_line = true;
+          blank_line = true; line_no++;
         }
         else if (c != '\r') {
-          // when finds a character on the current line
+          if ( (line_no == 0) && (l<79) && (c!='\n') && (c!='\r')) { url[l]=c; l++; url[l]=0;} // collect URL
           blank_line = false;
         }
       }
