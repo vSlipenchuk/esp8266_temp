6a9f231c2e79597916e5a40e75757af5b46a1482
Vladimir Slipenchuk <vslipenchuk@gmail.com>
2016-11-02 00:55:45 +0300

    added onboard DS1820 temp sensor

--- esp8266_temp.ino
+++ esp8266_temp.ino
@@ -6,10 +6,15 @@
 // Including the ESP8266 WiFi library
 #include <ESP8266WiFi.h>
 
-  // D1, D2, D3, D4, ->  GPIO 5, 4, 0, 2  (esp8266 digitalPins)
-  int D[4]={5,4,0,2};
+  // D1, D2, D3, D4, D5 D6 D7 D8 ->  GPIO 5, 4, 0, 2,   14,12,13,15 (esp8266 digitalPins)
+  int D[8]={5,4,0,2, 14,12,13,15};
 
 #include "DHT.h"
+#include <OneWire.h>
+
+OneWire ds(0); //pin4=D2
+float ds_celsius;
+//OneWire ds(13); //pin15=D8
 
 // Uncomment one of the lines below for whatever DHT sensor type you're using!
 #define DHTTYPE DHT11   // DHT 11
@@ -30,6 +35,7 @@ WiFiServer server(80);
 
 // DHT Sensor
 const int DHTPin = 5; // d1
+
 // Initialize DHT sensor.
 DHT dht(DHTPin, DHTTYPE);
 
@@ -44,12 +50,14 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  for(int i=0;i<4;i++) {
+  for(int i=0;i<4;i++) if (D[i]!=DHTPin)  {
+    
      pinMode(D[i],OUTPUT);
      digitalWrite(D[i],LOW);
      }
 
   dht.begin();
+  read_temp();
   
   // Connecting to WiFi network
   Serial.println();
@@ -74,8 +82,109 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+int read_ds() {
+    byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return 0;
+  }
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return 0;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return 0;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  ds_celsius = celsius; // remember
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+  return 1;
+}
 
 void read_temp() {
+  read_ds() || read_ds(); // try twice
+  
 float h = dht.readHumidity();
             // Read temperature as Celsius (the default)
             float t = dht.readTemperature();
@@ -139,7 +248,9 @@ flash_http200(client);
             client.println(fahrenheitTemp);
             client.println("*F</h3><h3>Humidity: ");
             client.println(humidityTemp);
-            client.println("%</h3><h3>");
+              client.println("%</h3><h3>OnBoard Temp: ");
+            client.println(ds_celsius);
+            client.println("*C</h3><h3>");
             client.println("</body></html>");     
 }
 
